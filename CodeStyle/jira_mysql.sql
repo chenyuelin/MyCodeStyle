12e678688ed4795b57f02b07c7c6427d3fdde332
cyl <cyl@DESKTOP-9OVT5H0>
2016-10-28 14:48:12 +0800

    更新sql文与url

--- CodeStyle/jira_mysql.sql
+++ CodeStyle/jira_mysql.sql
@@ -2,6 +2,8 @@ CREATE DATABASE jiradb CHARACTER SET utf8 COLLATE utf8_bin;
 
 GRANT ALL PRIVILEGES ON jiradb.* TO 'jira'@'localhost' IDENTIFIED by 'jira';
 
+SET PASSWORD FOR 'developer'@'%' = PASSWORD("developer"); 
+
 SHOW GRANTS FOR 'jira'@'localhost';
 
 SHOW GRANTS;
