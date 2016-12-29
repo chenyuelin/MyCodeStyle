6fab8d43269364e345f31952854666138efa255e
cyl <cyl@DESKTOP-9OVT5H0>
2016-12-29 16:05:23 +0800

    更新sql 文件

--- CodeStyle/jira_mysql.sql
+++ CodeStyle/jira_mysql.sql
@@ -1,3 +1,4 @@
+create database test3 DEFAULT CHARACTER SET gb18030 COLLATE gb18030_chinese_ci;
 CREATE DATABASE jiradb CHARACTER SET utf8 COLLATE utf8_bin;
 
 GRANT ALL PRIVILEGES ON jiradb.* TO 'jira'@'localhost' IDENTIFIED by 'jira';
